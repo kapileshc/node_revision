a263e35b1d1f7c35fd08b5870a35634b583d48a7
kapilesh <kapilesh1@gmail.com>
2022-09-09 20:48:15 +0530

    middleware creation

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const server = require('express')
 const app = server()
 
-app.get("/",(req,res)=>{
+
+const logger = (req,res,next) =>{
+    const method = req.method
+    const url = req.url
+    const time = new Date().getTime()
+    console.log(method,url,time)
+    next()
+}
+app.get("/",logger,(req,res)=>{
     res.send("Home")
 })
-app.get("/about",(req,res)=>{
+app.get("/about",logger,(req,res)=>{
     res.status(200).send("about")
 })
 
