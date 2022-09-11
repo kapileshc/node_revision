44ab4a358354e7f42abe8fcce31a68004dd64676
kapilesh <kapilesh1@gmail.com>
2022-09-11 20:26:47 +0530

    CRUD

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const {people} = require('./data')
 // app.get("/",logger,(req,res)=>{
 //     res.send("Home")
 // })
-// app.use(server.static('./static-folder'))
+app.use(server.static('./static-folder'))
+//parsing the form data
+app.use(server.urlencoded({extended: false }))
+// parsing the json data
+app.use(server.json())
+
 app.use([logger])
 // app.use function to pass middleware
 // app.use(logger)
@@ -27,11 +32,47 @@ app.get("/",(req,res)=>{
 app.get("/about",logger,(req,res)=>{
     res.status(200).send("about")
 })
+app.post('/login',(req,res)=>{
+    const {name} = req.body
+    // console.log(req.body.name)
+    name?res.status(200).send(`welcone ${name}`):res.status(401).send(`please provide correct credentials`)
+})
 
 app.get("/api/people",(req,res)=>{
     res.status(200).json({success:true,data:people})
 })
 
+app.post("/api/people",(req,res)=>{
+    const {name } = req.body
+    !name?res.status(400).json({success:false,msg:"pls provide the credentials"}):res.status(201).json({success:true})
+    
+})
+
+app.post("api/people/:id",(req,res)=>{
+    const {id } = req.params
+    const {name} = req.body
+    const person = people.find((person)=> person.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success:false,msg:`no person with id ${id}`})
+    }
+    const newPeople = people.map((person)=>{
+         if(person.id=== Number(id)){
+            person.name = name 
+         }
+         return person
+    })
+    res.status(200).json({success:true,data:newPeople})
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+     const person = people.find((person)=> person.id === Number(req.params.id))
+     if(!person){
+        return res.status(404).json({success:false,msg:`no person with id ${req.params.id}`})
+     }
+     const newPerson = people.filter((person)=> person.id !== Number(req.params.id))
+     return res.status(200).json({success:true,data:newPerson})
+})
 app.get("*",(req,res)=>{
     res.status(404).send("Page Not Found")
 })
